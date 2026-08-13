1c89e2ee42698c2e738928f5318109d902423a32
Su <ksh83lovesay@gmail.com>
2026-08-13 12:01:30 +0900

    Add files via upload

---
+++ countries.js
@@ -0,0 +1,802 @@
+window.COUNTRIES = [
+    {
+        "code":  "gh",
+        "name":  "가나",
+        "capital":  "Accra",
+        "lat":  8,
+        "lng":  -2,
+        "region":  "Africa"
+    },
+    {
+        "code":  "gt",
+        "name":  "과테말라",
+        "capital":  "Guatemala City",
+        "lat":  15.5,
+        "lng":  -90.25,
+        "region":  "Americas"
+    },
+    {
+        "code":  "gr",
+        "name":  "그리스",
+        "capital":  "Athens",
+        "lat":  39,
+        "lng":  22,
+        "region":  "Europe"
+    },
+    {
+        "code":  "ng",
+        "name":  "나이지리아",
+        "capital":  "Abuja",
+        "lat":  10,
+        "lng":  8,
+        "region":  "Africa"
+    },
+    {
+        "code":  "za",
+        "name":  "남아프리카",
+        "capital":  "Pretoria · Bloemfontein · Cape Town",
+        "lat":  -29,
+        "lng":  24,
+        "region":  "Africa"
+    },
+    {
+        "code":  "nl",
+        "name":  "네덜란드",
+        "capital":  "Amsterdam",
+        "lat":  52.5,
+        "lng":  5.75,
+        "region":  "Europe"
+    },
+    {
+        "code":  "np",
+        "name":  "네팔",
+        "capital":  "Kathmandu",
+        "lat":  28,
+        "lng":  84,
+        "region":  "Asia"
+    },
+    {
+        "code":  "no",
+        "name":  "노르웨이",
+        "capital":  "Oslo",
+        "lat":  62,
+        "lng":  10,
+        "region":  "Europe"
+    },
+    {
+        "code":  "nz",
+        "name":  "뉴질랜드",
+        "capital":  "Wellington",
+        "lat":  -41,
+        "lng":  174,
+        "region":  "Oceania"
+    },
+    {
+        "code":  "dk",
+        "name":  "덴마크",
+        "capital":  "Copenhagen",
+        "lat":  56,
+        "lng":  10,
+        "region":  "Europe"
+    },
+    {
+        "code":  "do",
+        "name":  "도미니카 공화국",
+        "capital":  "Santo Domingo",
+        "lat":  19,
+        "lng":  -70.66666666,
+        "region":  "Americas"
+    },
+    {
+        "code":  "de",
+        "name":  "독일",
+        "capital":  "Berlin",
+        "lat":  51,
+        "lng":  9,
+        "region":  "Europe"
+    },
+    {
+        "code":  "ru",
+        "name":  "러시아",
+        "capital":  "Moscow",
+        "lat":  60,
+        "lng":  100,
+        "region":  "Europe"
+    },
+    {
+        "code":  "lb",
+        "name":  "레바논",
+        "capital":  "Beirut",
+        "lat":  33.83333333,
+        "lng":  35.83333333,
+        "region":  "Asia"
+    },
+    {
+        "code":  "ro",
+        "name":  "루마니아",
+        "capital":  "Bucharest",
+        "lat":  46,
+        "lng":  25,
+        "region":  "Europe"
+    },
+    {
+        "code":  "ly",
+        "name":  "리비아",
+        "capital":  "Tripoli",
+        "lat":  25,
+        "lng":  17,
+        "region":  "Africa"
+    },
+    {
+        "code":  "mg",
+        "name":  "마다가스카르",
+        "capital":  "Antananarivo",
+        "lat":  -20,
+        "lng":  47,
+        "region":  "Africa"
+    },
+    {
+        "code":  "my",
+        "name":  "말레이시아",
+        "capital":  "Kuala Lumpur",
+        "lat":  2.5,
+        "lng":  112.5,
+        "region":  "Asia"
+    },
+    {
+        "code":  "mx",
+        "name":  "멕시코",
+        "capital":  "Mexico City",
+        "lat":  23,
+        "lng":  -102,
+        "region":  "Americas"
+    },
+    {
+        "code":  "ma",
+        "name":  "모로코",
+        "capital":  "Rabat",
+        "lat":  32,
+        "lng":  -5,
+        "region":  "Africa"
+    },
+    {
+        "code":  "mn",
+        "name":  "몽골국",
+        "capital":  "Ulan Bator",
+        "lat":  46,
+        "lng":  105,
+        "region":  "Asia"
+    },
+    {
+        "code":  "us",
+        "name":  "미국",
+        "capital":  "Washington D.C.",
+        "lat":  38,
+        "lng":  -97,
+        "region":  "Americas"
+    },
+    {
+        "code":  "mm",
+        "name":  "미얀마",
+        "capital":  "Naypyidaw",
+        "lat":  22,
+        "lng":  98,
+        "region":  "Asia"
+    },
+    {
+        "code":  "va",
+        "name":  "바티칸",
+        "capital":  "Vatican City",
+        "lat":  41.9,
+        "lng":  12.45,
+        "region":  "Europe"
+    },
+    {
+        "code":  "bd",
+        "name":  "방글라데시",
+        "capital":  "Dhaka",
+        "lat":  24,
+        "lng":  90,
+        "region":  "Asia"
+    },
+    {
+        "code":  "ve",
+        "name":  "베네수엘라",
+        "capital":  "Caracas",
+        "lat":  8,
+        "lng":  -66,
+        "region":  "Americas"
+    },
+    {
+        "code":  "vn",
+        "name":  "베트남",
+        "capital":  "Hanoi",
+        "lat":  16.16666666,
+        "lng":  107.83333333,
+        "region":  "Asia"
+    },
+    {
+        "code":  "be",
+        "name":  "벨기에",
+        "capital":  "Brussels",
+        "lat":  50.83333333,
+        "lng":  4,
+        "region":  "Europe"
+    },
+    {
+        "code":  "by",
+        "name":  "벨라루스",
+        "capital":  "Minsk",
+        "lat":  53,
+        "lng":  28,
+        "region":  "Europe"
+    },
+    {
+        "code":  "bo",
+        "name":  "볼리비아",
+        "capital":  "Sucre",
+        "lat":  -17,
+        "lng":  -65,
+        "region":  "Americas"
+    },
+    {
+        "code":  "bg",
+        "name":  "불가리아",
+        "capital":  "Sofia",
+        "lat":  43,
+        "lng":  25,
+        "region":  "Europe"
+    },
+    {
+        "code":  "br",
+        "name":  "브라질",
+        "capital":  "Brasília",
+        "lat":  -10,
+        "lng":  -55,
+        "region":  "Americas"
+    },
+    {
+        "code":  "ws",
+        "name":  "사모아",
+        "capital":  "Apia",
+        "lat":  -13.58333333,
+        "lng":  -172.33333333,
+        "region":  "Oceania"
+    },
+    {
+        "code":  "sa",
+        "name":  "사우디아라비아",
+        "capital":  "Riyadh",
+        "lat":  25,
+        "lng":  45,
+        "region":  "Asia"
+    },
+    {
+        "code":  "rs",
+        "name":  "세르비아",
+        "capital":  "Belgrade",
+        "lat":  44,
+        "lng":  21,
+        "region":  "Europe"
+    },
+    {
+        "code":  "sd",
+        "name":  "수단",
+        "capital":  "Khartoum",
+        "lat":  15,
+        "lng":  30,
+        "region":  "Africa"
+    },
+    {
+        "code":  "lk",
+        "name":  "스리랑카",
+        "capital":  "Colombo",
+        "lat":  7,
+        "lng":  81,
+        "region":  "Asia"
+    },
+    {
+        "code":  "se",
+        "name":  "스웨덴",
+        "capital":  "Stockholm",
+        "lat":  62,
+        "lng":  15,
+        "region":  "Europe"
+    },
+    {
+        "code":  "ch",
+        "name":  "스위스",
+        "capital":  "Bern",
+        "lat":  47,
+        "lng":  8,
+        "region":  "Europe"
+    },
+    {
+        "code":  "es",
+        "name":  "스페인",
+        "capital":  "Madrid",
+        "lat":  40,
+        "lng":  -4,
+        "region":  "Europe"
+    },
+    {
+        "code":  "sk",
+        "name":  "슬로바키아",
+        "capital":  "Bratislava",
+        "lat":  48.66666666,
+        "lng":  19.5,
+        "region":  "Europe"
+    },
+    {
+        "code":  "si",
+        "name":  "슬로베니아",
+        "capital":  "Ljubljana",
+        "lat":  46.11666666,
+        "lng":  14.81666666,
+        "region":  "Europe"
+    },
+    {
+        "code":  "sg",
+        "name":  "싱가포르",
+        "capital":  "Singapore",
+        "lat":  1.36666666,
+        "lng":  103.8,
+        "region":  "Asia"
+    },
+    {
+        "code":  "ae",
+        "name":  "아랍에미리트",
+        "capital":  "Abu Dhabi",
+        "lat":  24,
+        "lng":  54,
+        "region":  "Asia"
+    },
+    {
+        "code":  "ar",
+        "name":  "아르헨티나",
+        "capital":  "Buenos Aires",
+        "lat":  -34,
+        "lng":  -64,
+        "region":  "Americas"
+    },
+    {
+        "code":  "is",
+        "name":  "아이슬란드",
+        "capital":  "Reykjavik",
+        "lat":  65,
+        "lng":  -18,
+        "region":  "Europe"
+    },
+    {
+        "code":  "ht",
+        "name":  "아이티",
+        "capital":  "Port-au-Prince",
+        "lat":  19,
+        "lng":  -72.41666666,
+        "region":  "Americas"
+    },
+    {
+        "code":  "ie",
+        "name":  "아일랜드",
+        "capital":  "Dublin",
+        "lat":  53,
+        "lng":  -8,
+        "region":  "Europe"
+    },
+    {
+        "code":  "af",
+        "name":  "아프가니스탄",
+        "capital":  "Kabul",
+        "lat":  33,
+        "lng":  65,
+        "region":  "Asia"
+    },
+    {
+        "code":  "dz",
+        "name":  "알제리",
+        "capital":  "Algiers",
+        "lat":  28,
+        "lng":  3,
+        "region":  "Africa"
+    },
+    {
+        "code":  "ec",
+        "name":  "에콰도르",
+        "capital":  "Quito",
+        "lat":  -2,
+        "lng":  -77.5,
+        "region":  "Americas"
+    },
+    {
+        "code":  "et",
+        "name":  "에티오피아",
+        "capital":  "Addis Ababa",
+        "lat":  8,
+        "lng":  38,
+        "region":  "Africa"
+    },
+    {
+        "code":  "gb",
+        "name":  "영국",
+        "capital":  "London",
+        "lat":  54,
+        "lng":  -2,
+        "region":  "Europe"
+    },
+    {
+        "code":  "at",
+        "name":  "오스트리아",
+        "capital":  "Vienna",
+        "lat":  47.33333333,
+        "lng":  13.33333333,
+        "region":  "Europe"
+    },
+    {
+        "code":  "jo",
+        "name":  "요르단",
+        "capital":  "Amman",
+        "lat":  31,
+        "lng":  36,
+        "region":  "Asia"
+    },
+    {
+        "code":  "ug",
+        "name":  "우간다",
+        "capital":  "Kampala",
+        "lat":  1,
+        "lng":  32,
+        "region":  "Africa"
+    },
+    {
+        "code":  "uy",
+        "name":  "우루과이",
+        "capital":  "Montevideo",
+        "lat":  -33,
+        "lng":  -56,
+        "region":  "Americas"
+    },
+    {
+        "code":  "uz",
+        "name":  "우즈베키스탄",
+        "capital":  "Tashkent",
+        "lat":  41,
+        "lng":  64,
+        "region":  "Asia"
+    },
+    {
+        "code":  "ua",
+        "name":  "우크라이나",
+        "capital":  "Kyiv",
+        "lat":  49,
+        "lng":  32,
+        "region":  "Europe"
+    },
+    {
+        "code":  "iq",
+        "name":  "이라크",
+        "capital":  "Baghdad",
+        "lat":  33,
+        "lng":  44,
+        "region":  "Asia"
+    },
+    {
+        "code":  "ir",
+        "name":  "이란",
+        "capital":  "Tehran",
+        "lat":  32,
+        "lng":  53,
+        "region":  "Asia"
+    },
+    {
+        "code":  "il",
+        "name":  "이스라엘",
+        "capital":  "Jerusalem",
+        "lat":  31.47,
+        "lng":  35.13,
+        "region":  "Asia"
+    },
+    {
+        "code":  "eg",
+        "name":  "이집트",
+        "capital":  "Cairo",
+        "lat":  27,
+        "lng":  30,
+        "region":  "Africa"
+    },
+    {
+        "code":  "it",
+        "name":  "이탈리아",
+        "capital":  "Rome",
+        "lat":  42.83333333,
+        "lng":  12.83333333,
+        "region":  "Europe"
+    },
+    {
+        "code":  "in",
+        "name":  "인도",
+        "capital":  "New Delhi",
+        "lat":  20,
+        "lng":  77,
+        "region":  "Asia"
+    },
+    {
+        "code":  "id",
+        "name":  "인도네시아",
+        "capital":  "Jakarta",
+        "lat":  -5,
+        "lng":  120,
+        "region":  "Asia"
+    },
+    {
+        "code":  "jp",
+        "name":  "일본",
+        "capital":  "Tokyo",
+        "lat":  36,
+        "lng":  138,
+        "region":  "Asia"
+    },
+    {
+        "code":  "jm",
+        "name":  "자메이카",
+        "capital":  "Kingston",
+        "lat":  18.25,
+        "lng":  -77.5,
+        "region":  "Americas"
+    },
+    {
+        "code":  "kp",
+        "name":  "조선",
+        "capital":  "Pyongyang",
+        "lat":  40,
+        "lng":  127,
+        "region":  "Asia"
+    },
+    {
+        "code":  "cn",
+        "name":  "중국",
+        "capital":  "Beijing",
+        "lat":  35,
+        "lng":  105,
+        "region":  "Asia"
+    },
+    {
+        "code":  "cz",
+        "name":  "체코",
+        "capital":  "Prague",
+        "lat":  49.75,
+        "lng":  15.5,
+        "region":  "Europe"
+    },
+    {
+        "code":  "cl",
+        "name":  "칠레",
+        "capital":  "Santiago",
+        "lat":  -30,
+        "lng":  -71,
+        "region":  "Americas"
+    },
+    {
+        "code":  "cm",
+        "name":  "카메룬",
+        "capital":  "Yaoundé",
+        "lat":  6,
+        "lng":  12,
+        "region":  "Africa"
+    },
+    {
+        "code":  "kz",
+        "name":  "카자흐스탄",
+        "capital":  "Astana",
+        "lat":  48,
+        "lng":  68,
+        "region":  "Asia"
+    },
+    {
+        "code":  "qa",
+        "name":  "카타르",
+        "capital":  "Doha",
+        "lat":  25.5,
+        "lng":  51.25,
+        "region":  "Asia"
+    },
+    {
+        "code":  "kh",
+        "name":  "캄보디아",
+        "capital":  "Phnom Penh",
+        "lat":  13,
+        "lng":  105,
+        "region":  "Asia"
+    },
+    {
+        "code":  "ca",
+        "name":  "캐나다",
+        "capital":  "Ottawa",
+        "lat":  60,
+        "lng":  -95,
+        "region":  "Americas"
+    },
+    {
+        "code":  "ke",
+        "name":  "케냐",
+        "capital":  "Nairobi",
+        "lat":  1,
+        "lng":  38,
+        "region":  "Africa"
+    },
+    {
+        "code":  "cr",
+        "name":  "코스타리카",
+        "capital":  "San José",
+        "lat":  10,
+        "lng":  -84,
+        "region":  "Americas"
+    },
+    {
+        "code":  "co",
+        "name":  "콜롬비아",
+        "capital":  "Bogotá",
+        "lat":  4,
+        "lng":  -72,
+        "region":  "Americas"
+    },
+    {
+        "code":  "cu",
+        "name":  "쿠바",
+        "capital":  "Havana",
+        "lat":  21.5,
+        "lng":  -80,
+        "region":  "Americas"
+    },
+    {
+        "code":  "hr",
+        "name":  "크로아티아",
+        "capital":  "Zagreb",
+        "lat":  45.16666666,
+        "lng":  15.5,
+        "region":  "Europe"
+    },
+    {
+        "code":  "tz",
+        "name":  "탄자니아",
+        "capital":  "Dodoma",
+        "lat":  -6,
+        "lng":  35,
+        "region":  "Africa"
+    },
+    {
+        "code":  "th",
+        "name":  "태국",
+        "capital":  "Bangkok",
+        "lat":  15,
+        "lng":  100,
+        "region":  "Asia"
+    },
+    {
+        "code":  "tr",
+        "name":  "터키",
+        "capital":  "Ankara",
+        "lat":  39,
+        "lng":  35,
+        "region":  "Asia"
+    },
+    {
+        "code":  "tn",
+        "name":  "튀니지",
+        "capital":  "Tunis",
+        "lat":  34,
+        "lng":  9,
+        "region":  "Africa"
+    },
+    {
+        "code":  "pa",
+        "name":  "파나마",
+        "capital":  "Panama City",
+        "lat":  9,
+        "lng":  -80,
+        "region":  "Americas"
+    },
+    {
+        "code":  "py",
+        "name":  "파라과이",
+        "capital":  "Asunción",
+        "lat":  -23,
+        "lng":  -58,
+        "region":  "Americas"
+    },
+    {
+        "code":  "pk",
+        "name":  "파키스탄",
+        "capital":  "Islamabad",
+        "lat":  30,
+        "lng":  70,
+        "region":  "Asia"
+    },
+    {
+        "code":  "pg",
+        "name":  "파푸아뉴기니",
+        "capital":  "Port Moresby",
+        "lat":  -6,
+        "lng":  147,
+        "region":  "Oceania"
+    },
+    {
+        "code":  "pe",
+        "name":  "페루",
+        "capital":  "Lima",
+        "lat":  -10,
+        "lng":  -76,
+        "region":  "Americas"
+    },
+    {
+        "code":  "pt",
+        "name":  "포르투갈",
+        "capital":  "Lisbon",
+        "lat":  39.5,
+        "lng":  -8,
+        "region":  "Europe"
+    },
+    {
+        "code":  "pl",
+        "name":  "폴란드",
+        "capital":  "Warsaw",
+        "lat":  52,
+        "lng":  20,
+        "region":  "Europe"
+    },
+    {
+        "code":  "fr",
+        "name":  "프랑스",
+        "capital":  "Paris",
+        "lat":  46,
+        "lng":  2,
+        "region":  "Europe"
+    },
+    {
+        "code":  "fj",
+        "name":  "피지",
+        "capital":  "Suva",
+        "lat":  -18,
+        "lng":  175,
+        "region":  "Oceania"
+    },
+    {
+        "code":  "fi",
+        "name":  "핀란드",
+        "capital":  "Helsinki",
+        "lat":  64,
+        "lng":  26,
+        "region":  "Europe"
+    },
+    {
+        "code":  "ph",
+        "name":  "필리핀",
+        "capital":  "Manila",
+        "lat":  13,
+        "lng":  122,
+        "region":  "Asia"
+    },
+    {
+        "code":  "kr",
+        "name":  "한국",
+        "capital":  "Seoul",
+        "lat":  37,
+        "lng":  127.5,
+        "region":  "Asia"
+    },
+    {
+        "code":  "hu",
+        "name":  "헝가리",
+        "capital":  "Budapest",
+        "lat":  47,
+        "lng":  20,
+        "region":  "Europe"
+    },
+    {
+        "code":  "au",
+        "name":  "호주",
+        "capital":  "Canberra",
+        "lat":  -27,
+        "lng":  133,
+        "region":  "Oceania"
+    }
+];
---
+++ game.js
@@ -0,0 +1,152 @@
+const TOTAL_QUESTIONS = 10;
+const FEATURED_COUNTRY_CODES = [
+  "kr","jp","cn","kp","mn","lb","in","pk","bd","np","lk","th","vn","ph","id","my","sg","kh","mm","kz","uz","af","ir","iq","il","sa","ae","qa","jo","tr",
+  "gb","fr","de","it","es","pt","nl","be","ch","at","ie","dk","no","se","fi","is","pl","cz","sk","hu","ro","bg","gr","hr","rs","ua","ru","by","va","si",
+  "us","ca","mx","br","ar","cl","pe","co","ve","uy","py","ec","bo","cu","jm","do","cr","pa","gt","ht",
+  "eg","ma","dz","tn","ly","za","ng","gh","ke","et","tz","ug","sd","cm","mg",
+  "au","nz","fj","pg","ws"
+];
+const EXTRA_FAMILIAR_CODES = new Set(["kr","jp","cn","us","ca","gb","fr","de","it","es","br","au","in","th","vn"]);
+const letters = ["A", "B", "C", "D"];
+const capitalKo = {
+  kr:"서울",jp:"도쿄",cn:"베이징",mn:"울란바토르",kp:"평양",tw:"타이베이",us:"워싱턴 D.C.",ca:"오타와",mx:"멕시코시티",
+  br:"브라질리아",ar:"부에노스아이레스",cl:"산티아고",pe:"리마",co:"보고타",ve:"카라카스",uy:"몬테비데오",py:"아순시온",
+  bo:"수크레",ec:"키토",gb:"런던",fr:"파리",de:"베를린",it:"로마",es:"마드리드",pt:"리스본",nl:"암스테르담",
+  be:"브뤼셀",ch:"베른",at:"빈",ie:"더블린",dk:"코펜하겐",no:"오슬로",se:"스톡홀름",fi:"헬싱키",is:"레이캬비크",
+  pl:"바르샤바",cz:"프라하",sk:"브라티슬라바",hu:"부다페스트",ro:"부쿠레슈티",bg:"소피아",gr:"아테네",hr:"자그레브",
+  si:"류블랴나",rs:"베오그라드",ba:"사라예보",al:"티라나",mk:"스코페",me:"포드고리차",ua:"키이우",by:"민스크",
+  lt:"빌뉴스",lv:"리가",ee:"탈린",md:"키시너우",ru:"모스크바",tr:"앙카라",ge:"트빌리시",am:"예레반",az:"바쿠",
+  in:"뉴델리",pk:"이슬라마바드",bd:"다카",np:"카트만두",bt:"팀푸",lk:"스리자야와르데네푸라코테",mv:"말레",
+  th:"방콕",vn:"하노이",la:"비엔티안",kh:"프놈펜",mm:"네피도",my:"쿠알라룸푸르",sg:"싱가포르",id:"자카르타",
+  ph:"마닐라",bn:"반다르스리브가완",tl:"딜리",au:"캔버라",nz:"웰링턴",pg:"포트모르즈비",fj:"수바",
+  sa:"리야드",ae:"아부다비",qa:"도하",kw:"쿠웨이트시티",bh:"마나마",om:"무스카트",ye:"사나",jo:"암만",lb:"베이루트",
+  sy:"다마스쿠스",iq:"바그다드",ir:"테헤란",il:"예루살렘",ps:"라말라",kz:"아스타나",uz:"타슈켄트",tm:"아시가바트",
+  kg:"비슈케크",tj:"두샨베",af:"카불",eg:"카이로",ly:"트리폴리",tn:"튀니스",dz:"알제",ma:"라바트",sd:"하르툼",
+  et:"아디스아바바",ke:"나이로비",tz:"도도마",ug:"캄팔라",rw:"키갈리",bi:"기테가",so:"모가디슈",za:"프리토리아 · 케이프타운 · 블룸폰테인",
+  na:"빈트후크",bw:"가보로네",zw:"하라레",zm:"루사카",mz:"마푸투",ao:"루안다",cd:"킨샤사",cg:"브라자빌",ng:"아부자",
+  gh:"아크라",ci:"야무수크로",sn:"다카르",ml:"바마코",ne:"니아메",cm:"야운데",mg:"안타나나리보",mu:"포트루이스",
+  cu:"아바나",jm:"킹스턴",ht:"포르토프랭스",do:"산토도밍고",cr:"산호세",pa:"파나마시티",gt:"과테말라시티",
+  hn:"테구시갈파",sv:"산살바도르",ni:"마나과",bz:"벨모판",va:"바티칸 시국"
+};
+
+let quiz = [];
+let index = 0;
+let score = 0;
+let answered = false;
+let soundOn = true;
+const $ = (id) => document.getElementById(id);
+const intro = $("intro");
+const game = $("game");
+const result = $("result");
+
+function shuffle(list) {
+  const copy = [...list];
+  for (let i = copy.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [copy[i], copy[j]] = [copy[j], copy[i]];
+  }
+  return copy;
+}
+
+function playSound(kind) {
+  if (!soundOn) return;
+  try {
+    const AudioContextClass = window.AudioContext || window.webkitAudioContext;
+    if (!AudioContextClass) return;
+    const context = new AudioContextClass();
+    const oscillator = context.createOscillator();
+    const gain = context.createGain();
+    oscillator.connect(gain); gain.connect(context.destination);
+    const now = context.currentTime;
+    oscillator.type = kind === "wrong" ? "triangle" : "sine";
+    oscillator.frequency.setValueAtTime(kind === "correct" ? 523 : kind === "wrong" ? 190 : 350, now);
+    oscillator.frequency.linearRampToValueAtTime(kind === "correct" ? 784 : kind === "wrong" ? 130 : 460, now + .2);
+    gain.gain.setValueAtTime(.0001, now); gain.gain.exponentialRampToValueAtTime(.16, now + .02); gain.gain.exponentialRampToValueAtTime(.0001, now + .34);
+    oscillator.start(now); oscillator.stop(now + .35); oscillator.addEventListener("ended", () => context.close());
+  } catch (error) {
+    console.warn("효과음을 재생할 수 없습니다.", error);
+  }
+}
+
+function showScreen(name) {
+  intro.hidden = name !== "intro"; game.hidden = name !== "game"; result.hidden = name !== "result";
+  window.scrollTo({ top: 0, behavior: "smooth" });
+}
+
+function makeQuiz() {
+  const featured = window.COUNTRIES.filter((country) => FEATURED_COUNTRY_CODES.includes(country.code));
+  const weighted = featured.map((country) => ({ country, key: Math.pow(Math.random(), 1 / (EXTRA_FAMILIAR_CODES.has(country.code) ? 2.2 : 1)) }));
+  const selected = weighted.sort((a, b) => b.key - a.key).slice(0, TOTAL_QUESTIONS).map((item) => item.country);
+  quiz = selected.map((country) => {
+    const regional = featured.filter((item) => item.code !== country.code && item.region === country.region);
+    const others = featured.filter((item) => item.code !== country.code && item.region !== country.region);
+    const distractors = shuffle([...shuffle(regional).slice(0, 2), ...shuffle(others)]).slice(0, 3);
+    return { ...country, options: shuffle([country, ...distractors]) };
+  });
+}
+
+function flagPath(code) { return `assets/flags/${code}.png`; }
+function capitalName(country) { return capitalKo[country.code] || country.capital; }
+function regionName(region) { return { Africa:"아프리카", Americas:"아메리카", Asia:"아시아", Europe:"유럽", Oceania:"오세아니아", Antarctic:"남극" }[region] || "세계"; }
+
+function renderQuestion() {
+  const country = quiz[index]; answered = false;
+  $("quiz-view").hidden = false;
+  $("question-number").textContent = index + 1; $("score").textContent = score;
+  $("progress-bar").style.width = `${((index + 1) / TOTAL_QUESTIONS) * 100}%`;
+  $("progress-track").setAttribute("aria-valuenow", index + 1);
+  $("flag").src = flagPath(country.code); $("flag").alt = `${index + 1}번 문제 국기`;
+  $("feedback").hidden = true; $("feedback").className = "feedback-card game-view";
+  const box = $("answers"); box.replaceChildren();
+  country.options.forEach((option, i) => {
+    const button = document.createElement("button"); button.type = "button"; button.className = "answer"; button.dataset.code = option.code;
+    const letter = document.createElement("span"); letter.className = "letter"; letter.textContent = letters[i];
+    const label = document.createElement("strong"); label.textContent = option.name;
+    const mark = document.createElement("span"); mark.className = "mark";
+    button.append(letter, label, mark);
+    button.addEventListener("click", () => chooseAnswer(option.code));
+    box.append(button);
+  });
+}
+
+function chooseAnswer(code) {
+  if (answered) return; answered = true;
+  const country = quiz[index]; const correct = code === country.code;
+  if (correct) { score++; $("score").textContent = score; }
+  [...$("answers").children].forEach((button) => {
+    button.disabled = true;
+    if (button.dataset.code === country.code) { button.classList.add("correct"); button.querySelector(".mark").textContent = "✓"; }
+    else if (button.dataset.code === code) { button.classList.add("wrong"); button.querySelector(".mark").textContent = "×"; }
+    else button.classList.add("muted");
+  });
+  const feedback = $("feedback"); feedback.className = `feedback-card game-view ${correct ? "correct" : "wrong"}`;
+  $("feedback-emoji").textContent = correct ? "🎉" : "💡";
+  $("feedback-message").textContent = correct ? "딩동댕! 정답이야!" : "괜찮아, 이번에 기억하면 돼!";
+  $("country-name").textContent = country.name; $("capital").textContent = capitalName(country);
+  $("answer-flag").src = flagPath(country.code);
+  $("answer-flag").alt = `${country.name} 국기`;
+  $("fact").textContent = `위치: ${regionName(country.region)}, 수도: ${capitalName(country)}`;
+  $("pin-label").textContent = country.name;
+  const x = Math.max(3, Math.min(97, ((country.lng + 180) / 360) * 100));
+  const y = Math.max(6, Math.min(96, ((90 - country.lat) / 180) * 100));
+  $("pin").style.left = `${x}%`; $("pin").style.top = `${y}%`;
+  $("map").setAttribute("aria-label", `세계지도에서 ${country.name}의 위치`);
+  $("next-button").querySelector("span").textContent = index === TOTAL_QUESTIONS - 1 ? "결과 보기" : "다음 문제";
+  $("quiz-view").hidden = true;
+  feedback.hidden = false;
+  feedback.focus({ preventScroll: true });
+  playSound(correct ? "correct" : "wrong");
+}
+
+function startGame() { index = 0; score = 0; makeQuiz(); renderQuestion(); showScreen("game"); playSound("next"); }
+function showResult() {
+  const message = score === 10 ? "완벽한 국기 박사!" : score >= 7 ? "대단한 세계 탐험가!" : score >= 4 ? "멋지게 탐험했어!" : "다음 여행은 더 잘할 거야!";
+  $("result-message").textContent = message; $("final-score").textContent = score;
+  $("result-copy").textContent = `유명한 100개 나라 중 새로운 국기로 다시 도전할 수 있어!`; showScreen("result");
+}
+
+document.querySelectorAll(".sound-toggle").forEach((button) => button.addEventListener("click", () => {
+  soundOn = !soundOn; document.querySelectorAll(".sound-toggle").forEach((item) => { item.textContent = soundOn ? "🔊" : "🔇"; item.setAttribute("aria-label", soundOn ? "소리 끄기" : "소리 켜기"); });
+}));
+$("start-button").addEventListener("click", startGame); $("restart-button").addEventListener("click", startGame);
+$("next-button").addEventListener("click", () => { playSound("next"); if (index === TOTAL_QUESTIONS - 1) showResult(); else { index++; renderQuestion(); window.scrollTo({ top: 0, behavior: "smooth" }); } });
